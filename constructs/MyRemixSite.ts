482ede3d1d6013a6f29e30ec2bf7373f34437485
Daniel Haro Contreras <daniel@appfluence.com>
2023-04-12 17:54:42 +0200

    Simplify invalidating CloudFront root in every deploy

--- constructs/MyRemixSite.ts
+++ constructs/MyRemixSite.ts
@@ -1,58 +1,12 @@
-import { RemixSite, Stack } from "sst/constructs";
-import { CustomResource } from "aws-cdk-lib";
-import { Effect, Policy, PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { randomUUID } from "crypto";
+import { RemixSite } from "sst/constructs";
+
 // @ts-ignore
 export class MyRemixSite extends RemixSite {
-  private createCloudFrontInvalidation() {
-    const stack = Stack.of(this) as Stack;
-
-    const policy = new Policy(this, "CloudFrontInvalidatorPolicy", {
-      statements: [
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: [
-            "cloudfront:GetInvalidation",
-            "cloudfront:CreateInvalidation",
-          ],
-          resources: [
-            // @ts-ignore
-            `arn:${stack.partition}:cloudfront::${stack.account}:distribution/${this.distribution.distributionId}`,
-          ],
-        }),
-      ],
-    });
-    stack.customResourceHandler.role?.attachInlinePolicy(policy);
-
-    const publicInvalidator = new CustomResource(
-      this,
-      "CloudFrontInvalidator",
-      {
-        serviceToken: stack.customResourceHandler.functionArn,
-        resourceType: "Custom::CloudFrontInvalidator",
-        properties: {
-          buildId: this.generateBuildId(),
-          // @ts-ignore
-          distributionId: this.distribution.distributionId,
-          paths: ["/*"],
-          waitForInvalidation: this.props.waitForInvalidation,
-        },
-      }
-    );
-    publicInvalidator.node.addDependency(policy);
-    const ssgInvalidator = new CustomResource(this, "CloudFrontInvalidator2", {
-      serviceToken: stack.customResourceHandler.functionArn,
-      resourceType: "Custom::CloudFrontInvalidator",
-      properties: {
-        buildId: randomUUID(),
-        // @ts-ignore
-        distributionId: this.distribution.distributionId,
-        paths: ["/"],
-        waitForInvalidation: this.props.waitForInvalidation,
-      },
-    });
-    ssgInvalidator.node.addDependency(policy);
-
-    return [publicInvalidator, ssgInvalidator];
+  protected initBuildConfig() {
+    const res = super.initBuildConfig();
+    return {
+      ...res,
+      clientBuildVersionedSubDir: "this_does_not_exist_123",
+    };
   }
 }
